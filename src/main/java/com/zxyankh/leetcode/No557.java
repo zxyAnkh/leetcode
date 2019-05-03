1892c1e4fc0542ca9f64851c4ffc3d998ee67c21
zxyankh <zxyankh@gmail.com>
2019-05-03 12:03:39 +0800

    add some problems

---
+++ src/main/java/com/zxyankh/leetcode/No557.java
@@ -0,0 +1,32 @@
+package com.zxyankh.leetcode;
+
+/**
+ * 给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "Let's take LeetCode contest"
+ * 输出: "s'teL ekat edoCteeL tsetnoc"
+ * 注意：在字符串中，每个单词由单个空格分隔，并且字符串中不会有任何额外的空格。
+ *
+ * @author ankh
+ * @since 03.05.2019
+ */
+public class No557 {
+
+    public String reverseWords(String s) {
+        String[] strings = s.split(" ");
+        StringBuilder stringBuilder = new StringBuilder(s.length());
+        for (int i = 0; i < strings.length; i++) {
+            char[] chs = strings[i].toCharArray();
+            for (int j = chs.length - 1; j >= 0; j--) {
+                stringBuilder.append(chs[j]);
+            }
+            if (i != strings.length - 1) {
+                stringBuilder.append(' ');
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+}
---
+++ src/main/java/com/zxyankh/leetcode/No965.java
@@ -0,0 +1,55 @@
+package com.zxyankh.leetcode;
+
+/**
+ * 如果二叉树每个节点都具有相同的值，那么该二叉树就是单值二叉树。
+ * <p>
+ * 只有给定的树是单值二叉树时，才返回 true；否则返回 false。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：[1,1,1,1,1,null,1]
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：[2,2,2,5,2]
+ * 输出：false
+ *
+ * @author ankh
+ * @since 02.05.2019
+ */
+public class No965 {
+
+    public boolean isUnivalTree(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isUnivalTree(root.left, root.val) && isUnivalTree(root.right, root.val);
+    }
+
+    public boolean isUnivalTree(TreeNode root, int x) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val != x) {
+            return false;
+        }
+        return isUnivalTree(root.left, x) && isUnivalTree(root.right, x);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
